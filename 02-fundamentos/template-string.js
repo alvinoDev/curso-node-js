8a5799de1db66fe927a1aed7957cf155ec1be43a
alvinoDev <48383924+alvinoDev@users.noreply.github.com>
2024-04-03 16:37:37 -0400

    Feature: Templates literales

---
+++ 02-fundamentos/template-string.js
@@ -0,0 +1,17 @@
+const name = 'Superman';
+const identity = 'Clark Kent';
+
+const superHero = 'La identidad de ' + name + ' es ' + identity + '.';
+
+const template = `La identidad de ${name} es ${identity}.`;
+
+console.log(superHero);
+console.log(template);
+
+
+const html = `
+<h1>Hello</h1>
+<p>World!</p>
+`;
+
+console.log(html);
