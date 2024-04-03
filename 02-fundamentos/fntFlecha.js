5c5809ab3ceba96f03a95a2994b2e11be36a8a11
alvinoDev <48383924+alvinoDev@users.noreply.github.com>
2024-04-03 17:58:39 -0400

    Feature: Funciones de flecha

---
+++ 02-fundamentos/fntFlecha.js
@@ -0,0 +1,20 @@
+// Funciones tradicionales
+
+// function add(a, b) {
+//     return a + b;
+// }
+// console.log( add(5, 8));
+
+/* ============================ FUNCIONES FLECHA ============================ */
+// const add = (a, b) => {
+//     return a + b;
+// }
+// console.log( add(8, 8));
+
+// Forma acortada solo para esta accion
+
+const add = (a, b) => a + b;
+console.log(add(10, 8));
+
+const greeting = () => 'Hello World!';
+console.log(greeting());
